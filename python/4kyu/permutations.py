5f51eec7070f49cfe0f5821ce8251e0f88273843
drawbu <69208565+drawbu@users.noreply.github.com>
2022-11-12 02:10:08 +0100

    :zap: Fastest execution without recursion

--- python/4kyu/permutations.py
+++ python/4kyu/permutations.py
@@ -1,23 +1,14 @@
 # https://www.codewars.com/kata/5254ca2719453dcc0b00027d
 
 
-def recur_permutations(s: str, i: int = 0) -> list[str]:
-    return (
-        [s] if i + 2 > len(s)
-        else (
-                recur_permutations(s, i + 1) +
-                recur_permutations(s[:i] + s[i + 1] + s[i] + s[i + 2:], i + 1)
-        )
-    )
-
-
 def permutations(s: str) -> list[str]:
-    result = recur_permutations(s)
-    for _ in range(len(s)):
-        for e in result:
-            result += recur_permutations(e)
-            result = list(set(result))
-    return list(set(result))
+    result = [s]
+    for e in result:
+        for i in range(len(e)-1):
+            perm = e[:i] + e[i + 1] + e[i] + e[i + 2:]
+            if perm not in result:
+                result.append(perm)
+    return result
 
 
 def test_permutations() -> None:
@@ -29,8 +20,8 @@ def test_permutations() -> None:
         "aabb", "abab", "abba", "baab", "baba", "bbaa"
     ]
     assert sorted(permutations("abcd")) == [
-        'abcd', 'abdc', 'acbd', 'acdb', 'adbc', 'adcb', 'bacd', 'badc', 'bcad',
-        'bcda', 'bdac', 'bdca', 'cabd', 'cadb', 'cbad', 'cbda', 'cdab', 'cdba',
-        'dabc', 'dacb', 'dbac', 'dbca', 'dcab', 'dcba'
+        "abcd", "abdc", "acbd", "acdb", "adbc", "adcb", "bacd", "badc",
+        "bcad", "bcda", "bdac", "bdca", "cabd", "cadb", "cbad", "cbda",
+        "cdab", "cdba", "dabc", "dacb", "dbac", "dbca", "dcab", "dcba",
     ]
-    assert timeit.timeit(lambda: permutations("abdefgh"), number=1) < 1
+    assert timeit.timeit(lambda: permutations("abdefgh"), number=1) < 0.5
