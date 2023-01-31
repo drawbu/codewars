6b683d66d74d6e45668c8f841981c7a9d4f2c092
drawbu <69208565+drawbu@users.noreply.github.com>
2023-01-31 11:21:54 +0100

    :sparkles: Solution for C kata : Messi goal func

---
+++ src/c/messi_goals_function.c
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/c
+
+int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+{
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
